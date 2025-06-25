7ab8d91ed86a7c44e4f7c0a4ed37d4c6485921c1
GVersolatto <gustavo.versolatto@gmail.com>
2025-06-25 19:24:21 -0300

    adicionando modelo de usuario

--- backend/src/main/java/com/lacosdepelo/backend/models/UserModel.java
+++ backend/src/main/java/com/lacosdepelo/backend/models/UserModel.java
@@ -1,4 +1,64 @@
 package com.lacosdepelo.backend.models;
 
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
+@Entity
+@Table(name = "usuario")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @JsonIgnore // USADO PARA NAO PASSAR A SENHA NO JSON
+    private String password;
+
+    public UserModel() {
+    }
+
+    public UserModel(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
