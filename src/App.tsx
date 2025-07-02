21137427fc023ffb2a870e2890e3f1ee6b40d8f9
Caio Onha Ferreira <caioonha@gmail.com>
2025-07-02 19:23:44 -0300

    Fazendo carrinho (terminando...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { ShoppingCart } from "lucide-react"
+import Badge from "./components/ui/Badge";
 
 const App = () => {
     return (
-        <div className="flex items-center justify-between p-4">
-            <div className="bg-white p-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
-                <ShoppingCart size={30}/>
-            </div>
+        <div className="">
+            <Badge />
         </div>
     );
 }
---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { ShoppingCart } from "lucide-react";
+import { X } from "lucide-react";
+
+interface produtos {
+    id: number;
+    nome: string;
+    preco: number;
+}
+
+const produtos: produtos[] = [
+    { id: 1, nome: "Produto 1", preco: 10 },
+    { id: 2, nome: "Produto 2", preco: 20 },
+    { id: 3, nome: "Produto 3", preco: 30 },
+];
+
+const Badge = () => {
+    const [carrinho, setCarrinho] = useState<produtos[]>([]);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const addCarrinho = (produto: produtos) => {
+        setCarrinho((prevCarrinho) => [...prevCarrinho, produto]);
+    }
+
+    const removeCarrinho = (produtoId: number) => {
+        setCarrinho((prevCarrinho) => prevCarrinho.filter((produto) => produto.id !== produtoId))
+    }
+
+    const total = carrinho.reduce((sum, item) => sum + item.preco, 0);
+
+    return (
+        <div>
+            <button onClick={() => setSidebarOpen(true)} className="p-2 rounded-full hover:bg-gray-200">
+                <ShoppingCart size={30}/>
+                {carrinho.length > 0 && (
+                    <span className="absolute top-0 right-0 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center transform translate-x-2 -translate-y-2">
+                        {carrinho.length}
+                    </span>
+                )}
+            </button>
+
+            {sidebarOpen && (
+                <div className="fixed insert-0 bg-black bg-opacity-50 z-40" onClick={() => setSidebarOpen(false)}>
+                </div>
+            )}
+
+            {/* Sidebar */}
+            <aside className={`fixed top-0 right-0 w-1/3 h-full bg-white shadow-lg p-4 transition-transform ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                <div>
+                    <h2>
+                        Carrinho
+                    </h2>
+                    <button onClick={() => setSidebarOpen(false)}>
+                        <X size={24} />
+                    </button>
+                </div>
+            </aside>
+        </div>
+        
+    )
+}
+
+export default Badge;
