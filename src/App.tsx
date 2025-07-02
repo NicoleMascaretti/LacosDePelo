a42b99983d7b6a2fbb68692e288ff4d6f7e6babb
Caio Onha Ferreira <caioonha@gmail.com>
2025-07-01 22:13:37 -0300

    Testando

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { CodeXml } from "lucide-react"
+import { ShoppingCart } from "lucide-react"
 
 const App = () => {
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">Olá</h1>
-            <p className="text-lg mb-6">Testando...</p>
-            <CodeXml className="w-16 h-16 text-blue-500 mx-auto" />
-        </div>
+        <div className="flex items-center justify-between p-4">
+            <div className="bg-white p-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+                <ShoppingCart size={30}/>
+            </div>
         </div>
     );
 }
