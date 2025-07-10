dfed83f18714c6f3a6c78f11c6ab1db5f49d4d21
Caio Onha Ferreira <caioonha@gmail.com>
2025-07-10 18:16:27 -0300

    Atualizando UserMenu

--- src/components/ui/UserMenu.tsx
+++ src/components/ui/UserMenu.tsx
@@ -1,14 +1,13 @@
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-
+import { User } from "lucide-react";
 
 const UserMenu = () => {
     const [mensage, setMensage] = useState<void>();
     console.log(mensage);
     return (
         <div className="flex items-center justify-end p-4">
-            <button className="rounded-md bg-teal-400 text-white p-2 hover:bg-teal-700 font-sans font-bold" onClick={() => {setMensage(alert("Bem-vindo ao Lacos de Pelo!"));}}>
-                Minha conta
+            <button className="rounded-3xl hover:bg-gray-100 p-2 transition-colors" onClick={() => {setMensage(alert("Bem-vindo ao Lacos de Pelo!"));}} title="minha conta">
+                <User size={24}/>
             </button>
         </div>
     )
