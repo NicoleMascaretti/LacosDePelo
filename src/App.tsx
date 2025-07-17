905b16b2baa148f923750d0bc8c867d05fa997f5
Caio Onha Ferreira <caioonha@gmail.com>
2025-07-17 20:06:37 -0300

    Colocando props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FavoritesProvider } from "@/contexts/FavoritesContext";
-import React from "react";
-import { ShoppingCart } from "lucide-react"
-import Badge from "./components/ui/ShoppingCartWidget";
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { FavoritesProvider } from "@/contexts/FavoritesContext";
+// import React from "react";
+// import { ShoppingCart } from "lucide-react"
+// import Badge from "./components/ui/ShoppingCartWidget";
+
+import CardCategoria from "./components/ui/CardCategoria";
+import img1 from "../public/Cachorro1.jpg"
 
 const App = () => {
     return (
-        <div className="">
-            <Badge />
+        <div className="flex gap-20 mt-5 ms-1 me-1">
+            <CardCategoria img={img1} icon={"🥘"} titulo={"Ração e alimentação"} sub={"Rações premium e snaks"}/>
+            <CardCategoria img={img1} icon={"🎾"} titulo={"Brinquedos"} sub={"Diversão garantida"}/>
+            <CardCategoria img={img1} icon={"🛁"} titulo={"Higiêne e beleza"} sub={"Cuidados essenciais"}/>
         </div>
     );
 }
--- src/components/ui/CardCategoria.tsx
+++ src/components/ui/CardCategoria.tsx
@@ -1,19 +1,19 @@
 import photo from "../../../public/Cachorro1.jpg"
 
-const CardCategoria = () => {
+const CardCategoria = ({img, icon, titulo, sub}) => {
     return (
         <div className="w-96 bg-white rounded-xl shadow-lg overflow-hidden font-sans cursor-pointer hover:-translate-y-2 transform-gpu duration-100">
             <div className="aspect-w-16 aspect-h-12">
-                <img src={photo} alt="" className="w-full h-40 object-cover"/>
+                <img src={img} alt="imagem produto" className="w-full h-40 object-cover"/>
             </div>
 
             <div className="p-8">
                 <div className="flex items-center gap-2 mb-1">
-                    <span className="text-3xl">🥘</span>
-                    <h2 className="font-bold text-xl">Ração e Alimentação</h2>
+                    <span className="text-3xl">{icon}</span>
+                    <h2 className="font-bold text-xl">{titulo}</h2>
                 </div>
                 <div>
-                    <p className="text-base text-gray-500">Rações premium e snacks</p>
+                    <p className="text-base text-gray-500">{sub}</p>
                 </div>
             </div>
         </div>
