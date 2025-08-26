65978c7a66864cc5050f613d2d8a8b5faea41735
Caio Onha Ferreira <caioonha@gmail.com>
2025-08-25 22:08:16 -0300

    Objeto produto

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -3,6 +3,21 @@ import { fetchProducts } from "../services/Api";
 import type { ProductType } from "../types/ProductType"; 
 import ProductCard from "../components/ui/ProductCard";
 
+const products: ProductType[] = [
+  {
+    id: 1,
+    name: "Ração Premium para Cães",
+    price: 129.9,
+    originalPrice: 159.9,
+    category: "Rações",
+    rating: 4.5,
+    reviews: 23,
+    image: "https://place-puppy.com/300x300",
+    inStock: true,
+    badge: "Mais Vendido",
+  }
+]
+
 export default function Produtos() {
   const [products, setProducts] = useState<ProductType[]>([]);
   const [loading, setLoading] = useState(true);
