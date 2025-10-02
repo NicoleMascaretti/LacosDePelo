60cdcefb5345f39a53562e102b73149745441c53
Nicole <nsmascaretti@gmail.com>
2025-10-01 23:09:30 -0300

    arrumando um negocio

--- src/components/ui/InfoHome.tsx
+++ src/components/ui/InfoHome.tsx
@@ -2,7 +2,7 @@ const InfoHome = () => {
     return (
         <div className="flex justify-center gap-32">
             <div className="text-center">
-                <h1 className="font-bold text-3xl text-teal-600">5k+</h1>
+                <h1 className="font-bold text-3xl text-teal-600">100+</h1>
                 <p className="text-gray-600">Produtos</p>
             </div>
             <div className="text-center">
