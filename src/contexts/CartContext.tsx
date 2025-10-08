dd386fb1128c9f38f86f1cfe9d09cdc80b5acce6
unknown <gustavo.versolatto@gmail.com>
2025-10-08 18:15:21 -0300

    retirando linha não usada

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 import type { ReactNode } from "react";
 import type { ProductType } from "../types/ProductType";
 
-type VariantAwareId = { productId: string; variantId?: string };
+// type VariantAwareId = { productId: string; variantId?: string };
 
 export interface CartItem extends ProductType {
   quantity: number;
